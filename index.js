d96d282b0db618d743e781cc28c17f0330ad521d
Luis Alejandro Martínez Faneyth <luis@luisalejandro.org>
2021-08-22 16:18:23 -0400

    [REF] Improving readme.

--- index.js
+++ index.js
@@ -7,18 +7,19 @@ fs.writeFileSync(
   `
 ![About Me](https://github.com/LuisAlejandro/LuisAlejandro/blob/master/banner.svg)
 
-I'm a Telecom Engineer, currentlty working as a Sr Full Stack Developer at [@GuayoyoCyber](https://github.com/GuayoyoCyber). I'm also the founder of @CollageLabs, a tech startup on early stage. I'm passionate about finding out end user needs and creating beautiful interfaces with sustainable architecture. I've been on the field for more than 12 years but I'm always fascinated by new technology, which I try to integrate into new projects as soon as possible. I'm a huge advocate of mental health in our field.
+I'm a Telecom Engineer, currentlty working as a Sr Full Stack Developer at [@GuayoyoCyber](https://github.com/GuayoyoCyber). I'm also the founder of [@CollageLabs](https://github.com/CollageLabs), a tech startup on early stage. I'm passionate about finding out end user needs and creating beautiful interfaces with sustainable architecture. I've been on the field for more than 12 years but I'm always fascinated by new technology, which I try to integrate into new projects as soon as possible. I'm also a huge advocate of mental health in our field: products for humans should be made by humans behaving humanly.
 
 ### 🛠 &nbsp; On my toolbox
 
 ${config.SKILLS.map(({ logo, label, color, labelColor, logoColor }) => {
-  return `<img src="https://img.shields.io/badge/-${label}-FFFFFF?style=flat&logo=${logo}&color=${color}&labelColor=${labelColor}&logoColor=${logoColor}"/>&nbsp;`;
+  return `<img src="https://img.shields.io/static/v1?message=${label}&label=&style=flat&logo=${logo}&color=${color}&labelColor=${labelColor}&logoColor=${logoColor}"/>&nbsp;`;
 }).join("")}
 
 ### 🤝🏻 &nbsp; Get in touch
 
+
 ${config.SOCIAL.map(({ link, logo, label, color, labelColor, logoColor }) => {
-  return `<a href="${link}"><img src="https://img.shields.io/badge/-${label}-FFFFFF?style=flat&logo=${logo}&color=${color}&labelColor=${labelColor}&logoColor=${logoColor}"/></a>&nbsp;`;
+  return `<a href="${link}"><img src="https://img.shields.io/static/v1?message=${label}&label=&style=flat&logo=${logo}&color=${color}&labelColor=${labelColor}&logoColor=${logoColor}"/></a>&nbsp;`;
 }).join("")}
 `
 );
