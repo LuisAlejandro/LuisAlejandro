5f35504a6514bd385b9758adfeb7622166b8c435
Luis Alejandro Martínez Faneyth <luis@luisalejandro.org>
2022-03-12 18:59:03 -0400

    [REF] Improving visual look.

--- index.js
+++ index.js
@@ -9,20 +9,14 @@ fs.writeFileSync(
 
 I'm a Telecom Engineer, founder of [@CollageLabs](https://github.com/CollageLabs): a tech startup on early stage.
 
-I'm passionate 💖 about finding out end user needs and creating beautiful interfaces with sustainable architecture.
-
-I've been on the field for more than ${new Date().getFullYear() - config.STARTYEAR} years but I'm always fascinated by new technology, which I try to integrate into new projects as soon as possible.
+I'm passionate 💖 about finding out end user needs and creating beautiful interfaces with sustainable architecture. I've been on the field for more than ${new Date().getFullYear() - config.STARTYEAR} years but I'm always fascinated by new technology, which I try to integrate into new projects as soon as possible.
 
 I'm also a huge advocate of mental health 🧠 in our field: products for humans should be made by humans behaving as if they were human (or at least pretending to act like one).
 
 ### 🛠 &nbsp; On my toolbox
 
-${config.SKILLS.map(({ label, items }) => {
-  const skillset = `#### ${label}`;
-  const skill = items.map(({ logo, label, color, labelColor, logoColor }) => {
-    return `<img src="https://img.shields.io/static/v1?message=${label}&label=&style=flat&logo=${logo}&color=${color}&labelColor=${labelColor}&logoColor=${logoColor}"/>&nbsp;`;
-  }).join("");
-  return `${skillset}\n\n${skill}\n\n`;
+${config.SKILLS.map(({ logo, label, color, labelColor, logoColor }) => {
+  return `<img src="https://img.shields.io/static/v1?message=${label}&label=&style=flat&logo=${logo}&color=${color}&labelColor=${labelColor}&logoColor=${logoColor}"/>&nbsp;`;
 }).join("")}
 
 ### 🤝🏻 &nbsp; Get in touch
